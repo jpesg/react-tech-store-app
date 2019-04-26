3d18874e0edc788115bf94078a2e19eaee8290c1
jpesg <jpesgil@gmail.com>
2019-04-26 19:39:08 +0100

    Context configuration

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import { ProductConsumer } from "../context";
+
 export default class HomePage extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>home Page</h1>
+        <ProductConsumer>{value => <h1>value</h1>}</ProductConsumer>
       </React.Fragment>
     );
   }

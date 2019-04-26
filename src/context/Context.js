1c4353feb2a074959a1b38063cfc5d48ed2599da
jpesg <jpesgil@gmail.com>
2019-04-26 20:30:44 +0100

    navbar

--- src/context/Context.js
+++ src/context/Context.js
@@ -4,9 +4,43 @@ const ProductContext = React.createContext();
 //Provider
 //Consumer
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0
+  };
+
+  //handle sidebar
+  handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+
+  //handle side Cart
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen });
+  };
+
+  //handle close cart
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+
+  //handle open cart
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ export default class HomePage extends Component {
   render() {
     return (
       <React.Fragment>
-        <ProductConsumer>{value => <h1>value</h1>}</ProductConsumer>
+        <ProductConsumer>{value => <h1>home page</h1>}</ProductConsumer>
       </React.Fragment>
     );
   }

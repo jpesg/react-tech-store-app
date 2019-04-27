316c8c9c680a5b00e2c66c0cb82633532378f99c
jpesg <jpesgil@gmail.com>
2019-04-27 19:00:07 +0100

    DefaultPage with hero component

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import defaultBcg from "../images/defaultBcg.jpeg";
+
 export default class DefaultPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Default Page</h1>
+        <Hero title="404" max="true" img={defaultBcg}>
+          <h2 className="text-uppercase">page not found</h2>
+          <Link to="/" className="main-link" style={{ marginTop: "2rem" }}>
+            return home
+          </Link>
+        </Hero>
       </React.Fragment>
     );
   }

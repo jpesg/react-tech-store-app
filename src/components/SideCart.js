d49188bfd588c45bd0052a44cc94b45f3c843a09
jpesg <jpesgil@gmail.com>
2019-04-27 17:11:14 +0100

    sidecart component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { ProductConsumer } from "../context";
+import styled from "styled-components";
 
 export default function SideCart() {
-  return <div>Sidecart</div>;
+  return (
+    <ProductConsumer>
+      {value => {
+        const { cartOpen, closeCart, openCart, cart } = value;
+        return (
+          <CartWrapper show={cartOpen} onClick={closeCart}>
+            <p>cart items</p>
+          </CartWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 }
+
+const CartWrapper = styled.div`
+  position: fixed;
+  top: 60px;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: var (--mainGrey);
+  z-index: 1;
+  border-left: 4px solid var(--primaryColor);
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
+  transform: ${props => (props.show ? "translateX(0)" : "translate(100%)")};
+  transition: var(--mainTransition);
+`;

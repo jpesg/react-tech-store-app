d3bbaa402b0de47d0aa5be4bd8e6abfeb188efa8
jpesg <jpesgil@gmail.com>
2019-05-03 19:30:26 +0100

    decrement and clearcart method in context

--- src/context/Context.js
+++ src/context/Context.js
@@ -206,21 +206,26 @@ class ProductProvider extends Component {
     console.log(`decrement: ${id}`);
     let tempCart = [...this.state.cart];
     const cartItem = tempCart.find(item => item.id === id);
-    cartItem.count--;
-    cartItem.total = cartItem.count * cartItem.price;
-    cartItem.total = parseFloat(cartItem.total.toFixed(2));
 
-    this.setState(
-      () => {
-        return {
-          cart: [...tempCart]
-        };
-      },
-      () => {
-        this.addTotals();
-        this.syncStorage();
-      }
-    );
+    cartItem.count--;
+    if (cartItem.count === 0) {
+      this.removeItem(id);
+    } else {
+      cartItem.total = cartItem.count * cartItem.price;
+      cartItem.total = parseFloat(cartItem.total.toFixed(2));
+
+      this.setState(
+        () => {
+          return {
+            cart: [...tempCart]
+          };
+        },
+        () => {
+          this.addTotals();
+          this.syncStorage();
+        }
+      );
+    }
   };
   //remove item
   removeItem = id => {
@@ -241,6 +246,15 @@ class ProductProvider extends Component {
   //clearCart
   clearCart = () => {
     console.log(`clearCart`);
+    this.setState(
+      {
+        cart: []
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+      }
+    );
   };
 
   render() {
